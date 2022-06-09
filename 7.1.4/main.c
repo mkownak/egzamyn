2cac15569b5e2cc9fb875e4828004840f956f115
mkownak <92052799+mkownak@users.noreply.github.com>
2022-06-09 21:01:50 +0200

    Update main.c

--- 7.1.4/main.c
+++ 7.1.4/main.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct Pracownik
+ 
+struct node
 {
-    char *imie;
-    int nr;
-    float wynagrodzenie;
+    int a;
+    struct node * next;
 };
-
-int max(struct Pracownik tab[], int n)
+ 
+int foo(struct node *lista)
 {
-    int max=tab[0].nr;
-    for(int i=0; i<n; i++)
+    struct node *wsk=lista->next;
+    while(wsk!=NULL)
     {
-        if(tab[i].nr>max)
+        if(wsk->a%2!=0)
         {
-            max=tab[i].nr;
+            return &wsk->a;
         }
+        wsk=wsk->next;
     }
-    return max;
+    return NULL;
 }
-
+ 
 int main()
 {
-    struct Pracownik P1;
-    P1.imie="Adam";
-    P1.nr=6;
-    P1.wynagrodzenie=100.01;
-
-    struct Pracownik P2;
-    P2.imie="Andrzej";
-    P2.nr=10;
-    P2.wynagrodzenie=105.02;
-
-    struct Pracownik P3;
-    P3.imie="Adam";
-    P3.nr=21;
-    P3.wynagrodzenie=110.03;
-
-    struct Pracownik tab[3]={P1,P2,P3};
-
-    printf("%i",max(tab,3));
-
+    struct node *lista=malloc(sizeof(struct node));
+    lista->next=NULL;
+ 
+    struct node *wsk=malloc(sizeof(struct node));
+    wsk->a=4;
+    wsk->next=lista->next;
+    lista->next=wsk;
+ 
+    struct node *wsk2=malloc(sizeof(struct node));
+    wsk2->a=5;
+    wsk2->next=lista->next;
+    lista->next=wsk2;
+ 
+    struct node *wsk3=malloc(sizeof(struct node));
+    wsk3->a=6;
+    wsk3->next=lista->next;
+    lista->next=wsk3;
+ 
+    printf("%p",foo(lista));
+ 
     return 0;
 }
